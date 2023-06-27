b6523ea49e495e11124e2a7b88c9d466bce21644
T-the-Moose <prioux76@hotmail.fr>
2023-06-27 15:21:45 +0200

    Modif
    Impossible de récup l'id Utilisateur dans la DAO

--- src/main/java/fr/eni/siteEncheres/dal/UtilisateurDaoSqlServerImpl.java
+++ src/main/java/fr/eni/siteEncheres/dal/UtilisateurDaoSqlServerImpl.java
@@ -17,67 +17,46 @@ import fr.eni.siteEncheres.bo.Utilisateur;
 public class UtilisateurDaoSqlServerImpl implements UtilisateurDAO {
 	
 	private final static String SELECT_ALL = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, credit  FROM UTILISATEURS";
-//	private final static String FIND_BY_ID = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, credit FROM UTILISATEURS WHERE no_utilisateur=:no_utilisateur";
+	private final static String FIND_BY_ID = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, credit FROM UTILISATEURS WHERE no_utilisateur=:no_utilisateur";
 	
 	@Autowired
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
-	private JdbcTemplate jdbcTemplate;
 	
-	@Override
-    public Utilisateur read(Integer idUtilisateur) {    
-
-       String sql = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
-
-       Utilisateur utilisateur = jdbcTemplate.queryForObject(sql, new Object[]{idUtilisateur}, new Integer[] { java.sql.Types.INTEGER }, new RowMapper<Utilisateur>() {
-
-       @Override
-
-       public Utilisateur mapRow(ResultSet rs, int rowNum) throws SQLException {
-           // Récupérer les valeurs des colonnes du film
-           Integer id = rs.getInt("no_utilisateur");
-           String pseudo = rs.getString("pseudo");
-           String nom = rs.getString("nom");
-           String prenom = rs.getString("prenom");
-           String email = rs.getString("email");
-           String telephone = rs.getString("telephone");
-           String rue = rs.getString("rue");
-           String codePostal = rs.getString("code_postal");
-           String ville = rs.getString("ville");
-           Integer credit = rs.getInt("credit");
-
-           // Créer et retourner l'objet Film avec les valeurs récupérées
-           return new Utilisateur(id, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, credit);
-           }
-       });
-       return utilisateur;
-    }
+	class UtilisateurMapper implements RowMapper<Utilisateur> {
+		
+		@Override
+		public Utilisateur mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Utilisateur utilisateur = new Utilisateur();
+			
+			utilisateur.setIdUtilisateur(rs.getInt("no_utilisateur"));
+			utilisateur.setPseudo(rs.getString("pseudo"));
+			utilisateur.setNom(rs.getString("nom"));
+			utilisateur.setPrenom(rs.getString("prenom"));
+			utilisateur.setEmail(rs.getString("email"));
+			utilisateur.setTelephone(rs.getString("telephone"));
+			utilisateur.setRue(rs.getString("rue"));
+			utilisateur.setCodePostal(rs.getString("code_postal"));
+			utilisateur.setVille(rs.getString("ville"));
+			utilisateur.setCredit(rs.getInt("credit"));
+			
+			System.out.println(utilisateur);
+			
+			return utilisateur;
+		}
+	}
 	
+	// Methode findById
+	NamedParameterJdbcTemplate t;
 	
+	public Utilisateur read (Integer idUtilisateur) {
+		t = namedParameterJdbcTemplate;
+	    
+	    System.out.println("L id de l uilisateur est " + idUtilisateur);
+	    
+		return (Utilisateur) t.getJdbcOperations().query(FIND_BY_ID, new UtilisateurMapper(), idUtilisateur );
+	}
 	
-//	class UtilisateurMapper implements RowMapper<Utilisateur> {
-//		
-//		@Override
-//		public Utilisateur mapRow(ResultSet rs, int rowNum) throws SQLException {
-//			Utilisateur utilisateur = new Utilisateur();
-//			
-//			utilisateur.setIdUtilisateur(rs.getInt("no_utilisateur"));
-//			utilisateur.setPseudo(rs.getString("pseudo"));
-//			utilisateur.setNom(rs.getString("nom"));
-//			utilisateur.setPrenom(rs.getString("prenom"));
-//			utilisateur.setEmail(rs.getString("email"));
-//			utilisateur.setTelephone(rs.getString("telephone"));
-//			utilisateur.setRue(rs.getString("rue"));
-//			utilisateur.setCodePostal(rs.getString("code_postal"));
-//			utilisateur.setVille(rs.getString("ville"));
-//			utilisateur.setCredit(rs.getInt("credit"));
-////			
-//			System.out.println(utilisateur);
-//			
-//			return utilisateur;
-//		}
-//	}
-//	
 //	@Override
 //	public Utilisateur read(Integer idUtilisateur) {
 //		MapSqlParameterSource paramSrc = new MapSqlParameterSource ("no_utilisateur", idUtilisateur);
@@ -96,4 +75,32 @@ public class UtilisateurDaoSqlServerImpl implements UtilisateurDAO {
 	}
 	
 	
+//	@Override
+//    public Utilisateur read(Integer idUtilisateur) {    
+//
+//       String sql = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
+//
+//       Utilisateur utilisateur = jdbcTemplate.queryForObject(sql, new Object[]{idUtilisateur}, new int[] { java.sql.Types.INTEGER }, new RowMapper<Utilisateur>() {
+//
+//       @Override
+//
+//       public Utilisateur mapRow(ResultSet rs, int rowNum) throws SQLException {
+//           // Récupérer les valeurs des colonnes du film
+//           Integer id = rs.getInt("no_utilisateur");
+//           String pseudo = rs.getString("pseudo");
+//           String nom = rs.getString("nom");
+//           String prenom = rs.getString("prenom");
+//           String email = rs.getString("email");
+//           String telephone = rs.getString("telephone");
+//           String rue = rs.getString("rue");
+//           String codePostal = rs.getString("code_postal");
+//           String ville = rs.getString("ville");
+//           Integer credit = rs.getInt("credit");
+//
+//           // Créer et retourner l'objet Film avec les valeurs récupérées
+//           return new Utilisateur(id, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, credit);
+//           }
+//       });
+//       return utilisateur;
+//    }
 }
