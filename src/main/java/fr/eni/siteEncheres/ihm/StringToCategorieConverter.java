189566112f1ed63d4e4d7e05cf05acfedeaf3466
T-the-Moose <prioux76@hotmail.fr>
2023-06-29 17:13:43 +0200

    Modif
    Modif StringToCategorieConverter

--- src/main/java/fr/eni/siteEncheres/ihm/StringToCategorieConverter.java
+++ src/main/java/fr/eni/siteEncheres/ihm/StringToCategorieConverter.java
@@ -1,26 +1,26 @@
-//package fr.eni.siteEncheres.ihm;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.stereotype.Component;
-//
-//import fr.eni.siteEncheres.bll.CategorieServiceImpl;
-//import fr.eni.siteEncheres.bo.Categorie;
-//
-//@Component
-//public class StringToCategorieConverter implements Converter<String, Categorie>{
-//
-//	private CategorieServiceImpl service;
-//	
-//	@Autowired
-//	public void setCategorieService(CategorieServiceImpl service) {
-//		this.service = service;
-//	}
-//	
-//	@Override
-//	public Categorie convert(String idCateogire) {
-//		Integer idCategorie = Integer.parseInt(idCateogire);
-//		return service.findById(idCategorie);
-//	}
-//	
-//}
+package fr.eni.siteEncheres.ihm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import fr.eni.siteEncheres.bll.CategorieServiceImpl;
+import fr.eni.siteEncheres.bo.Categorie;
+
+@Component
+public class StringToCategorieConverter implements Converter<String, Categorie>{
+
+	private CategorieServiceImpl service;
+	
+	@Autowired
+	public void setCategorieService(CategorieServiceImpl service) {
+		this.service = service;
+	}
+	
+	@Override
+	public Categorie convert(String idCateogire) {
+		Integer idCategorie = Integer.parseInt(idCateogire);
+		return service.findById(idCategorie);
+	}
+	
+}
